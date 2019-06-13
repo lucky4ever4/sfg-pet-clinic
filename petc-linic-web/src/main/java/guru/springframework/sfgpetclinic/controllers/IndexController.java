6db5aa12e98655d3ff45d7c49c1d02d45ba1937e
lucky4everApostol <cristyapostol93@gmail.com>
2019-06-13 12:35:45 +0300

    Updated base entity with JPA annotations

--- petc-linic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ petc-linic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -12,4 +12,12 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+
+
+    @RequestMapping("/oups")
+    public String oupsHandler() {
+
+        return "notimplemented";
+    }
 }
