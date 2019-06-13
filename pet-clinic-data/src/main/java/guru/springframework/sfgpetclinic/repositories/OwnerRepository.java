d8f0052af46cd94c90ac12c6a45e334db49ccc61
lucky4everApostol <cristyapostol93@gmail.com>
2019-06-13 13:32:57 +0300

    added spring data jpa service for owners

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
+    Owner findByLastName(String lastName);
+
 }
