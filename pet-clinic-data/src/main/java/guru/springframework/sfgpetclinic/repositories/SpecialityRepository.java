603f473262faa8e49cbf9ad01cada7367d5c3a83
lucky4everApostol <cristyapostol93@gmail.com>
2019-06-13 13:16:19 +0300

    added JPA repositories for all POJO's

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialityRepository.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpecialityRepository extends CrudRepository<Speciality, Long> {
+
+
+}
